72cf7b823e62f543c871342403d8696a30dd74bc
Taylor Barnett <taylor@transposit.com>
2019-09-17 23:09:07 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -5,26 +5,34 @@
  * For sample code and reference material, visit
  * https://www.transposit.com/docs/building/webhooks
  */
-({ http_event }) => {
-  const parsed_body = http_event.parsed_body;
-  const workspaceId = parsed_body.team_id;
-  const userId = parsed_body.user_id;
-  const response_url = parsed_body.response_url;
+// ({ http_event }) => {
+//   const parsed_body = http_event.parsed_body;
+//   const workspaceId = parsed_body.team_id;
+//   const userId = parsed_body.user_id;
+//   const response_url = parsed_body.response_url;
 
-  setImmediate(() => {
-    let user = api.user({type: "slack", workspaceId, userId});
-    if (user) {
-      let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: message
-      });      
-    } else {
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: {text: 'Please configure your user at ' + env.getBuiltin().appUrl}
-      });      
-    }
-  });
-  return { status_code: 200 };
+//   setImmediate(() => {
+//     let user = api.user({type: "slack", workspaceId, userId});
+//     if (user) {
+//       let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
+//       api.run("slack_webhook.post_to_response_url", {
+//         response_url: response_url,
+//         post_body: message
+//       });      
+//     } else {
+//       api.run("slack_webhook.post_to_response_url", {
+//         response_url: response_url,
+//         post_body: {text: 'Please configure your user at ' + env.getBuiltin().appUrl}
+//       });      
+//     }
+//   });
+//   return { status_code: 200 };
+// }
+
+if (http_event.parsed_body.challenge) {
+  return {
+    status_code: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: body.challenge
+  };
 }
