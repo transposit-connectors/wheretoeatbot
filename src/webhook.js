34d4216b208aa9d9f3e3426fa23cca5d80e1aba6
Taylor Barnett <taylor@transposit.com>
2019-09-18 17:24:43 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -24,10 +24,9 @@
   let body_text = parsed_body.event.text;
   let lex_result = api.run("this.post_text", {slackText : body_text});
   
-  console.log(channelId);
   console.log(lex_result[0].message);
   
-  //let lex_slack = api.run("this.post_chat_message", {lexReponse : lex_result[0].message, });
+  //let lex_slack = api.run("this.post_chat_message", {lexReponse : lex_result[0].message, channelId : channelId});
     
 //   setImmediate(() => {
 //     let user = api.user({type: "slack", workspaceId, userId});
