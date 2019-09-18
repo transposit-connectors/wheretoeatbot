3becae3918c7a89dd3a6e0457b341872ce23da36
Taylor Barnett <taylor@transposit.com>
2019-09-18 21:50:33 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -19,7 +19,7 @@
     let body_text = parsed_body.event.text;    
     let lex_result = api.run("this.post_text", {slackText : body_text});
     
-    if (!lex_result[0].dialogState == "ReadyForFulfillment") {
+    if (lex_result[0].dialogState != "ReadyForFulfillment") {
     	console.log(lex_result);
     	let lex_slack = lex_result[0].message;
     	let lex_slack_response = api.run("this.post_chat_message", {lexResponse : lex_slack, channelId : channelId});      
