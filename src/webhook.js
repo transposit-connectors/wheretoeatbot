121a4190db23e891a5e47430d686c036f130bf6c
Taylor Barnett <taylor@transposit.com>
2019-09-19 16:13:15 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -21,7 +21,7 @@
         
     if (lex_result[0].dialogState != "ReadyForFulfillment") {
     	let lex_slack = lex_result[0].message;
-    	let lex_slack_response = api.run("this.post_chat_message", {lexResponse : lex_slack, channelId : channelId});      
+    	let lex_slack_response = api.run("this.post_chat_message", {text : lex_slack, channelId : channelId});      
     }
     else if (lex_result[0].dialogState === "ReadyForFulfillment"){
       	let maxPrice = lex_result[0]["slots"]["Price"];
@@ -29,7 +29,7 @@
       	// avg walk speed is 1.4 m/s, so max time in min * 1.4 * 60 to get maxDistance in meters
       	let maxDistance = lex_result[0]["slots"]["Distance"] * 1.4 * 60;
       
-    	let slack_recommendations = api.run("this.post_chat_message", {lexResponse : "I have some suggestions...", channelId : channelId});
+    	let slack_recommendations = api.run("this.post_chat_message", {text : "I have some suggestions...", channelId : channelId});
       	
       	let google_geocode_results = api.run("this.geocode", {address : userAddress});      
       	let latLong = [google_geocode_results[0]["results"][0]["geometry"]["location"]["lat"], 
@@ -39,7 +39,7 @@
         let google_place_results = api.run("this.search_nearby", {radius : maxDistance, location : latLong});
       
       	let google_message = google_place_results[0]["name"] + " " + google_place_results[0]["vicinity"];
-      	let google_slack = api.run("this.post_chat_message", {lexResponse : google_message, channelId : channelId});
+      	let google_slack = api.run("this.post_chat_message", {text : google_message, channelId : channelId});
     }
   } 
   return { status_code: 200 };
