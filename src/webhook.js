ba6a2903c6c863037208c5a9607c031e7e337a75
Taylor Barnett <taylor@transposit.com>
2019-09-19 21:45:40 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -40,8 +40,9 @@
           let google_message = "I have a suggestion... \n";
           Object.keys(google_place_results.forEach(function (item) {
             console.log(google_place_results);
+            // returns correct results
             console.log(google_place_results[item]);
-            // "null"
+            // returns "null"
             google_message += google_place_results[item]["name"] + " \n" + "https://www.google.com/maps/place/?q=place_id:" + google_place_results[item]["place_id"];
           })); 
           let google_slack = api.run("this.post_chat_message", {text : google_message, channelId : channelId});                                            
