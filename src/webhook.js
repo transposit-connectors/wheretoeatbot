3f5800a776c8378863138508598f96a0dea4a8de
Taylor Barnett <taylor@transposit.com>
2019-09-20 17:07:18 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -33,11 +33,12 @@
       	let latLong = [google_geocode_results[0]["results"][0]["geometry"]["location"]["lat"], 
                        google_geocode_results[0]["results"][0]["geometry"]["location"]["lng"]];
       	latLong = latLong.join();
-      	
+      	console.log(maxPrice);
       	let google_place_results = api.run("this.search_nearby", {radius : maxDistance, location : latLong, maxprice: 2});
             
 		if (Object.keys(google_place_results.length)) {
           let google_message = "I have some suggestions... \n";
+          console.log(google_place_results);
           Object.keys(google_place_results).forEach(function (item) {
             console.log(google_place_results[item]["name"]);
             google_message += google_place_results[item]["name"] + " \n" + 
