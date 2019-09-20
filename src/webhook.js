2c40f0266f2330e9f139fdbd4624d803ae476f08
Taylor Barnett <taylor@transposit.com>
2019-09-20 03:35:27 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -44,7 +44,7 @@
       	// // returns correct results
       	// console.log(google_place_results[item]);
       	// // returns "null"
-      	// fegoogle_message += google_place_results[item]["name"] + " \n" + "https://www.google.com/maps/place/?q=place_id:" + google_place_results[item]["place_id"];
+      	// google_message += google_place_results[item]["name"] + " \n" + "https://www.google.com/maps/place/?q=place_id:" + google_place_results[item]["place_id"];
       	// })); 
       	// let google_slack = api.run("this.post_chat_message", {text : google_message, channelId : channelId});                                            
       	// } else {
@@ -57,8 +57,11 @@
           let google_message = "I have some suggestions... \n";
           Object.keys(google_place_results).forEach(function (item) {
     		console.log(google_place_results[item]["name"]);
-    		google_message += google_place_results[item]["name"] + " ";
+    		google_message += google_place_results[item]["name"] + " \n" + "https://www.google.com/maps/place/?q=place_id:" + google_place_results[item]["place_id"];;
 		  });
+      	  let google_slack = api.run("this.post_chat_message", {text : google_message, channelId : channelId});                                            
+        } else {
+      	  let google_slack = api.run("this.post_chat_message", {text : "I'm sorry I don't have any suggestions within that distance", channelId : channelId});
         }
     }
   } 
